a7bdd2686caf7c679dead3fe0f65ba951b6553b9
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-03-20 12:57:08 +0530

    Awards Controller

--- app/Http/Controllers/Backend/AwardsController.php
+++ app/Http/Controllers/Backend/AwardsController.php
@@ -1,11 +1,14 @@
 <?php
 
-namespace App\Http\Controllers\Bakend;
+namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AwardsController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
 }
