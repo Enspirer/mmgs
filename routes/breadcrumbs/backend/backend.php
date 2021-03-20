feb162aa3cdf1c59f4b48fea28b289a946d73022
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-03-20 10:39:35 +0530

    Backend Breaccrums

--- routes/breadcrumbs/backend/backend.php
+++ routes/breadcrumbs/backend/backend.php
@@ -4,5 +4,20 @@ Breadcrumbs::for('admin.dashboard', function ($trail) {
     $trail->push(__('strings.backend.dashboard.title'), route('admin.dashboard'));
 });
 
+Breadcrumbs::for('admin.projects.index', function ($trail) {
+    $trail->push('Projects', route('admin.projects.index'));
+});
+
+Breadcrumbs::for('admin.projects.create', function ($trail) {
+    $trail->push('Projects Create', route('admin.projects.create'));
+});
+
+Breadcrumbs::for('admin.projects.edit', function ($trail) {
+    $trail->push('Projects Edit', route('admin.projects.edit',1));
+});
+
+
+
+
 require __DIR__.'/auth.php';
 require __DIR__.'/log-viewer.php';
