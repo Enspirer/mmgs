900b782292fa748e7bc5cf946329823983164566
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-03-25 14:16:43 +0530

    Grasicle

--- resources/views/frontend/gallery.blade.php
+++ resources/views/frontend/gallery.blade.php
@@ -20,17 +20,18 @@
                               <div class="row">
                                   @foreach($items as $itemS)
 
-                                    @if($itemS['effect'] == 'black_and_white')
+
                                         <div class="" style="filter: grayscale(1);">
                                             <div class="col-md-4"  data-toggle="modal" data-target="#exampleModal{{$itemS['image_id']}}">
-                                                <div class="" style="background-image: url('{{url('files/'.$itemS['image_name'])}}');height: 150px;background-position: center;background-size: contain"></div>
+                                                @if($itemS['effect'] == 'black_and_white')
+                                                <div class="" style="background-image: url('{{url('files/'.$itemS['image_name'])}}');height: 150px;background-position: center;background-size: contain;filter: grayscale(1)"></div>
+                                                @else
+                                                    <div class="" style="background-image: url('{{url('files/'.$itemS['image_name'])}}');height: 150px;background-position: center;background-size: contain"></div>
+                                                @endif
                                             </div>
                                         </div>
-                                    @else
-                                          <div class="col-md-4"  data-toggle="modal" data-target="#exampleModal{{$itemS['image_id']}}">
-                                              <div class="" style="background-image: url('{{url('files/'.$itemS['image_name'])}}');height: 150px;background-position: center;background-size: contain"></div>
-                                          </div>
-                                    @endif
+
+
 
 
                                   @endforeach
