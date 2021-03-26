6abfafb8978ac54b54976908fe9d50b33780016b
Kavindu Theekshana <kavindutheekshana@gmail.com>
2021-03-27 00:50:20 +0530

    Responsive

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,28 +3,10 @@
 @section('title', app_name() . ' | ' . __('navs.general.home'))
 
 @section('content')
-<section id="home-page-background" style="background-image: url('{{asset("img/assets/HOMEPAGEimage.jpg")}}');">
-      <div class="container">
-       @include('frontend.layouts.menu')
-
-        <div class="home-page-text" id="home-page-text">
-          <p class="web-p">
-            architecture is my <span>PASSION;</span> <br />
-            not just my profession.
-          </p>
-
-          <p class="mobile-p" style="display: none;">
-            architecture is <br> my <span>PASSION;</span> <br />
-            not just my profession.
-          </p>
-        </div>
-      </div>
-    </section>
-
-
-    <section id="home-page-background2"  style="background-image: url('{{asset("img/assets/HOMEPAGEimage2.jpg")}}');background-size: 100%;background-position: bottom !important;">
+<section id="home-page-background">
       <div class="container">
       @include('frontend.layouts.menu')
+
         <div class="home-page-text" id="home-page-text">
           <p class="web-p">
             architecture is my <span>PASSION;</span> <br />
@@ -39,6 +21,24 @@
       </div>
     </section>
 
+
+    <section id="home-page-background2" >
+      <div class="container">
+      @include('frontend.layouts.menu')
+
+        <div class="home-page-text" id="home-page-text">
+          <p class="web-p">
+            architecture is my <span>PASSION;</span> <br />
+            not just my profession.
+          </p>
+
+          <p class="mobile-p" style="display: none;">
+            architecture is <br> my <span>PASSION;</span> <br />
+            not just my profession.
+          </p>
+        </div>
+      </div>
+    </section>
 
 <!-- <section id="home-page-background" style="background-image: url('{{asset("img/assets/HOMEPAGEimage.jpg")}}')">
     <div class="container">
