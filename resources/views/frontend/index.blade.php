dbe5ce063b7472a6ed1bd2ce6cd23615f8ee8611
Kavindu Theekshana <kavindutheekshana@gmail.com>
2021-03-27 22:53:08 +0530

    Home Page Menu

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,59 +3,46 @@
 @section('title', app_name() . ' | ' . __('navs.general.home'))
 
 @section('content')
-<section id="home-page-background">
-      <div class="container">
-      @include('frontend.layouts.menu')
 
-        <div class="home-page-text" id="home-page-text">
-          <p class="web-p">
-            architecture is my <span>PASSION;</span> <br />
-            not just my profession.
-          </p>
-
-          <p class="mobile-p" style="display: none;">
-            architecture is <br> my <span>PASSION;</span> <br />
-            not just my profession.
-          </p>
-        </div>
-      </div>
-    </section>
-
-
-    <section id="home-page-background2" >
-      <div class="container">
-      @include('frontend.layouts.menu')
-
-        <div class="home-page-text" id="home-page-text">
-          <p class="web-p">
-            architecture is my <span>PASSION;</span> <br />
-            not just my profession.
-          </p>
-
-          <p class="mobile-p" style="display: none;">
-            architecture is <br> my <span>PASSION;</span> <br />
-            not just my profession.
-          </p>
-        </div>
-      </div>
-    </section>
-
-<!-- <section id="home-page-background" style="background-image: url('{{asset("img/assets/HOMEPAGEimage.jpg")}}')">
-    <div class="container">
-        
-
+<section id="home-page-background2" style="z-index: 99999999;">
+  <div class="container">
     @include('frontend.layouts.menu')
-        <div class="home-page-text" id="home-page-text">
-            <p class="web-p">
-                architecture is my <span>PASSION;</span> <br />
-                not just my profession.
-            </p>
 
-            <p class="mobile-p" style="display: none;">
-                architecture is <br> my <span>PASSION;</span> <br />
-                not just my profession.
-            </p>
-        </div>
+    <div class="home-page-text" id="home-page-text">
+      <p class="web-p">
+        architecture is my <span>PASSION;</span> <br />
+        not just my profession.
+      </p>
+
+      <p class="mobile-p" style="display: none;">
+        architecture is <br> my <span>PASSION;</span> <br />
+        not just my profession.
+      </p>
     </div>
-</section> -->
+  </div>
+</section>
+
+
+<section id="home-page-background">
+  <div class="container">
+    @include('frontend.layouts.menu')
+
+    <div class="home-page-text" id="home-page-text">
+      <p class="web-p">
+        architecture is my <span>PASSION;</span> <br />
+        not just my profession.
+      </p>
+
+      <p class="mobile-p" style="display: none;">
+        architecture is <br> my <span>PASSION;</span> <br />
+        not just my profession.
+      </p>
+    </div>
+  </div>
+</section>
+
+
+
+
+
 @endsection
