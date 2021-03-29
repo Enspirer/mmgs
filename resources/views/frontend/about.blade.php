eb5f9483dd80dc0dc9392136201202121f7fb96b
Kavindu Theekshana <kavindutheekshana@gmail.com>
2021-03-29 10:51:07 +0530

    About us Text

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -91,37 +91,33 @@
 
             <div class="main-right-about col-md-12">
               <div class="row">
-                <div class="text-col">
+              <div class="text-col">
                   <br />
-                  <p class="about-paragraph" style="text-align: justify;">
+                  <p class="about-paragraph" style="text-align: justify;font-weight: 300;">
                     Mathew Mangaladaz Godridge Samuel (MMGS), born in Colombo in
                     1962, graduated from the University of Moratuwa Sri Lanka.
                     Became corporate member of the Sri Lanka Institute of
                     Architects (SLIA) in 1992. Elected a fellow member of SLIA
                     in 2003.
                   </p>
-                  <br>
-                  <p class="about-paragraph" style="text-align: justify;">
+                  <p class="about-paragraph" style="text-align: justify;font-weight: 300;">
                     Started his practice in the living room with a single
                     draughtsman in 1992, today the practice is known as MMGS
                     ARCHITECTS having its own design studio in central Colombo.
                   </p>
-                  <br>
-                  <p class="about-paragraph" style="text-align: justify;">
+                  <p class="about-paragraph" style="text-align: justify;font-weight: 300;">
                     MMGS ARCHITECTS, an award winning architectural practice in
                     Sri Lanka, comprising young and energetic architects,
                     experimenting with creative solutions for their clients.
                     Each project is a celebration of innovation, work of joy,
                     worthy addition to the contemporary Sri Lankan architecture.
                   </p>
-                  <br>
-                  <p class="about-paragraph" style="text-align: justify;">
+                  <p class="about-paragraph" style="text-align: justify;font-weight: 300;">
                     Winning number of design and color awards and a few design
                     competitions makes Godridge Samuel, a leading, award winning
                     architect in Sri Lanka.
                   </p>
-                  <br>
-                  <p class="about-paragraph" style="text-align: justify;">
+                  <p class="about-paragraph" style="text-align: justify;font-weight: 300;">
                     Today, Godridge Samuel has evolved a distinctive style of
                     his own ‘MODERN, MINIMALISTIC, TROPICAL, SUSTAINABLE
                     ARCHITECTURE’.
