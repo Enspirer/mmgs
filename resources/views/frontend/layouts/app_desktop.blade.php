cd03379338df01fd37bed40658d3baebba69df01
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-06-11 15:40:02 +0530

    Overflow Visibles

--- resources/views/frontend/layouts/app_desktop.blade.php
+++ resources/views/frontend/layouts/app_desktop.blade.php
@@ -56,14 +56,11 @@
 
 @yield('content')
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
 <script src='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js'></script>
 <script src='https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js'></script>
-
 
 <script    type="text/javascript">
     setTimeout(preloaderfunc, 3000)
@@ -72,7 +69,6 @@
         $('#preimage').hide(); // will first fade out the loading animation
         $('#preloader').delay(50); // will fade out the white DIV that covers the website.
         $('#preloader').hide(); // will fade out the white DIV that covers the website.
-        $('body').delay(50).css({'overflow':'visible'});
     }
 
 </script>
